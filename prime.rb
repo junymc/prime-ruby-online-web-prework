86f599c314fff37420ae138198e121b2a53cb981
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-04 19:08:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,15 @@
-def prime?(number)
- number.prime# Add  code here!
+# def prime?(number)
+#  number.prime# Add  code here!
+# end
+
+def is_prime?
+    prime = true
+    for r in 2..Math.sqrt(self).to_i
+      if (self % r == 0)
+        prime = false
+        break
+      end
+    end
+    return prime
+  end
 end
