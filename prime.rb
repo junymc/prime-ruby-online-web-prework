a7426ce3a12b784c23402da2628606ae1e345528
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-04 19:40:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,7 @@
 def prime?(n)
- n > 1
- (2..n-1).none?{|divisor| n % divisor == 0}# Add  code here!
+ if n > 1
+   return false
+ else
+   (2..n-1).none?{|divisor| n % divisor == 0}# Add  code here!
+ end
 end
